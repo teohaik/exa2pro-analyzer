12796736a1d4e23b977aa26b684b567bd9fd25bd
Theodore Chaikalis <teohaik@gmail.com>
2019-09-04 18:11:28 +0300

    initial

---
+++ src/main/java/gr/uom/exa2pro/git/analyzer/RepoFile.java
@@ -0,0 +1,33 @@
+package gr.uom.exa2pro.git.analyzer;
+
+import java.util.HashSet;
+
+public class RepoFile implements Comparable<RepoFile>{
+	
+	String path;
+	HashSet<String> commits;
+	
+	public RepoFile(String path) {
+		super();
+		this.path = path;
+		commits = new HashSet<>();
+	}
+	
+	public void addCommit(String commit) {
+		commits.add(commit);
+	}
+
+	@Override
+	public int compareTo(RepoFile other) {
+		if (this.commits.size() < other.commits.size() ) return 1;
+		return -1;
+	}
+
+	@Override
+	public String toString() {
+		return  path + "," + commits.size();
+	}
+	
+	
+
+}
